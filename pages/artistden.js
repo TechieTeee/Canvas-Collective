a61d013a799c5bc3cd194eb0a73d756f015d3f8a
TechieTeee <techieteee@protonmail.com>
2023-11-05 21:20:22 +0000

    Updated artist profile page

--- pages/artistden.js
+++ pages/artistden.js
@@ -11,28 +11,14 @@ export default function ArtistDen() {
     upcomingDeadlines: 'Deliver artwork for "Sunset Collection" by Nov 15',
   };
 
+  // Calculate the progress for current earnings and deadlines
+  const currentEarningsProgress = (artist.currentEarnings / artist.expectedEarnings) * 100;
+  // Assuming you want to show a progress bar for the upcoming deadline but it's not clear from the provided data, you can use a placeholder value.
+  const upcomingDeadlineProgress = 50; // 50% progress as an example
+
   return (
     <div className="bg-[#CFC8BE]">
-      {/* Navigation bar */}
-      <nav className="bg-[#FFDE59] p-4">
-        <div className="max-w-5xl mx-auto flex justify-between items-center">
-          <h1 className="text-2xl text-black font-bold">Canvas Collective</h1>
-          <ul className="flex space-x-4 items-center text-black">
-            <li className="cursor-pointer">Home</li>
-            <li className="cursor-pointer">About</li>
-            <li className="cursor-pointer">Check Out the Artists</li>
-            <li className="cursor-pointer">Collectors' Corner</li>
-            <Link href="/artistden">
-              <a>Artists' Den</a> {/* Use Link to navigate to artistden.js */}
-            </Link>
-            <li className="cursor-pointer">
-              <button className="bg-[#FF739D] hover:bg-[#E84F70] text-white font-bold py-2 px-4 rounded">
-                Sign In / Sign Up
-              </button>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      {/* ... (Navigation bar) ... */}
 
       {/* Main content of ArtistDen page */}
       <main className="relative flex min-h-screen flex-col items-center justify-between p-24">
@@ -52,6 +38,7 @@ export default function ArtistDen() {
         <div className="my-8 text-center">
           <h2 className="text-2xl font-semibold text-black">Earnings</h2>
           <p className="text-lg text-black">Current Earnings: ${artist.currentEarnings}</p>
+          <progress max="100" value={currentEarningsProgress}></progress>
           <p className="text-lg text-black">Expected Earnings: ${artist.expectedEarnings}</p>
         </div>
 
@@ -65,6 +52,18 @@ export default function ArtistDen() {
         <div className="my-8 text-center">
           <h2 className="text-2xl font-semibold text-black">Upcoming Deadlines</h2>
           <p className="text-lg text-black">{artist.upcomingDeadlines}</p>
+          <progress max="100" value={upcomingDeadlineProgress}></progress>
+        </div>
+
+        {/* Customer Testimonial */}
+        <div className="my-8 text-center">
+          <h2 className="text-2xl font-semibold text-black">Customer Testimonial</h2>
+          <div className="bg-white p-4 rounded-md shadow-lg">
+            <p className="text-lg text-gray-600">
+              "I recently purchased artwork from Ashley Rivers on Canvas Collective, and it has truly transformed my living space. Her art exudes a unique blend of creativity and emotion. I'm grateful to have found her work on this platform, and I can't wait to see what she creates next!"
+            </p>
+            <p className="text-md text-gray-400 mt-4">- Sarah, a satisfied Canvas Collective customer</p>
+          </div>
         </div>
       </main>
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function ArtistDen() {
-  // Artist information
-  const artist = {
-    name: 'Ashley Rivers',
-    currentEarnings: 5000,
-    expectedEarnings: 10000,
-    healthBenefits: 'Opolis Healthcare plan: platinum plan active until Dec 31st',
-    upcomingDeadlines: 'Deliver artwork for "Sunset Collection" by Nov 15',
-  };
-
+export default function Home() {
   return (
     <div className="bg-[#CFC8BE]">
       {/* Navigation bar */}
@@ -22,9 +13,7 @@ export default function ArtistDen() {
             <li className="cursor-pointer">About</li>
             <li className="cursor-pointer">Check Out the Artists</li>
             <li className="cursor-pointer">Collectors' Corner</li>
-            <Link href="/artistden">
-              <a>Artists' Den</a> {/* Use Link to navigate to artistden.js */}
-            </Link>
+            <li className="cursor-pointer">Artists' Den</li>
             <li className="cursor-pointer">
               <button className="bg-[#FF739D] hover:bg-[#E84F70] text-white font-bold py-2 px-4 rounded">
                 Sign In / Sign Up
@@ -33,44 +22,92 @@ export default function ArtistDen() {
           </ul>
         </div>
       </nav>
-
-      {/* Main content of ArtistDen page */}
+      
+      {/* Main content */}
       <main className="relative flex min-h-screen flex-col items-center justify-between p-24">
-        {/* Artist profile */}
+        {/* Hero section with image */}
+        <div className="hero text-center">
+          <Image
+            src="/pexels-los-muertos-crew-7205806.jpg"
+            alt="Hero Image"
+            width={1200}
+            height={800}
+          />
+          <div className="h-16" /> {/* Spacer with 16px height */}
+          <h1 className="text-6xl font-bold text-black mb-4">Canvas Collective</h1>
+          <p className="text-2xl text-black mb-8">
+            Where art, community, and financial security converge
+          </p>
+        </div>
+
+        {/* Problem Statement */}
+        <div className="problem-section my-16 text-center">
+          <h2 className="text-3xl font-semibold text-black mb-4">The Problem</h2>
+          <p className="text-2xl text-black">
+            The world of artists is characterized by financial hardship and uncertainty that transcends borders. A vast majority of artists worldwide grapple with the challenge of making a living from their craft, irrespective of cultural and language differences. This global issue calls for an all-encompassing solution that empowers artists and acknowledges their contributions, regardless of their location.
+
+            The prevalence of financial insecurity among artists is a harsh reality, with many struggling to make ends meet. A 2017 Artfinder study revealed that 75% of artists worldwide earned less than $10,000 annually from their art, and nearly half earned no more than $5,000. Female artists, in particular, face acute financial instability, with 83.6% earning less than $10,000. This lack of stable income takes a significant toll on artists' mental health, as a 2022 Record Union study found that 73% of independent musicians experienced stress, anxiety, and depression related to their work. These challenges are a universal experience for artists worldwide.
+
+            Despite their financial struggles, artists play a crucial role in enriching our lives and shaping our world. Their creative expressions serve as a source of inspiration, a medium for social commentary, and a bridge across cultures. They challenge our perspectives, expand our horizons, and connect us to our shared humanity.
+
+            To address the pressing challenges faced by artists globally, urgent solutions are needed to provide financial security, encourage collaboration, and expand access to opportunities. Universal Basic Income (UBI) has emerged as a potential solution, offering artists a regular, unconditional payment that could alleviate financial stress and enable them to focus on their craft.
+          </p>
+          <p className="text-2xl text-black">
+            In addition to UBI, initiatives that promote cross-border collaboration and provide artists with access to international markets can also play a significant role in addressing financial insecurity. Platforms like Canvas Collective can serve as hubs for artists to connect, share their work, and gain exposure to a global audience.
+
+            Empowering artists is not just about addressing their financial needs; it is about recognizing and celebrating their immense contributions to society. Artists are the voices of our communities, the storytellers of our time, and the visionaries who shape our future. They deserve support, nurturing, and the opportunity to thrive in their chosen field.
+
+            In a world where creativity knows no bounds, it is imperative to break down barriers and foster an environment where artists can flourish. By providing them with financial stability, access to opportunities, and a platform to showcase their work, we can unleash their creative potential and ensure that their talents shine brightly across the globe.
+          </p>
+        </div>
+
+        {/* Empowering Artists Worldwide */}
+        <div className="solution-section my-16 text-center">
+          <h2 className="text-3xl font-semibold text-black mb-4">Empowering Artists Worldwide</h2>
+          <p className="text-2xl text-black">
+            The road to recognition and financial stability can be arduous for talented artists. Connecting with art enthusiasts and showcasing their work often presents significant challenges. However, Canvas Collective emerges as a groundbreaking platform, offering artists a dependable income source reminiscent of agricultural agreements, but thoughtfully tailored for the art community.
+
+            Driven by strategic alliances and leveraging the cutting-edge technology of Near protocol and blockchain, Canvas Collective pioneers a pathway for artists to secure regular income. It defies geographical boundaries, ensuring that artists, whether based in the U.S. or elsewhere, stand on equal footing. They are united by the limitless potential for creative expression and the boundless opportunity for financial empowerment. At Canvas Collective, no artist's creativity should ever be confined by financial constraints or geographical borders.
+          </p>
+        </div>
+
+        <div className="h-16" /> {/* Spacer with 16px height */}
+        {/* Canvas Collective Community Pic */}
         <div className="text-center">
           <Image
-            src="/jake-nackos-IF9TK5Uy-KI-unsplash.jpg"
-            alt={artist.name}
-            width={200}
-            height={200}
-            className="rounded-full"
+            src="/pexels-matheus-bertelli-3856033.jpg"
+            alt="Centered Image"
+            width={880}
+            height={660}
           />
-          <h1 className="text-3xl font-semibold text-black mt-4">{artist.name}</h1>
         </div>
 
-        {/* Artist's earnings */}
-        <div className="my-8 text-center">
-          <h2 className="text-2xl font-semibold text-black">Earnings</h2>
-          <p className="text-lg text-black">Current Earnings: ${artist.currentEarnings}</p>
-          <p className="text-lg text-black">Expected Earnings: ${artist.expectedEarnings}</p>
+        {/* Explore Artists */}
+        <div className="checkout-artists my-16 text-center">
+          <h2 className="text-3xl font-semibold text-black mb-4">Check Out Our Artists</h2>
+          <p className="text-2xl text-black mb-8">
+            Explore a diverse collection of artworks from our talented artists.
+            Find the perfect piece to adorn your space or support your favorite creators.
+          </p>
+          <button className="bg-[#FFDE59] hover:bg-yellow-400 text-black font-bold py-2 px-4 rounded">
+            Check Out Our Artists
+          </button>
         </div>
 
-        {/* Health benefits */}
-        <div className="my-8 text-center">
-          <h2 className="text-2xl font-semibold text-black">Health Benefits</h2>
-          <p className="text-lg text-black">{artist.healthBenefits}</p>
+        <div className="text-center">
+          <Image
+            src="/pexels-joshua-poyer-2854589.jpg"
+            alt="Centered Image"
+            width={600}
+            height={450}
+          />
         </div>
 
-        {/* Upcoming deadlines */}
-        <div className="my-8 text-center">
-          <h2 className="text-2xl font-semibold text-black">Upcoming Deadlines</h2>
-          <p className="text-lg text-black">{artist.upcomingDeadlines}</p>
-        </div>
       </main>
 
       {/* Footer */}
       <footer className="bg-[#FFDE59] text-center py-4">
-        <p className="text-black">
+        <p className="text-black text-sm">
           &copy; 2023 Canvas Collective. All rights reserved.
         </p>
       </footer>
